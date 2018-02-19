f0a70571fc6df2c6a4424cffa7ebee7906eeacd4
Dev-And <raptor131296@gmail.com>
2018-02-19 13:32:25 +0300

    setup migrations

--- db/migrate/20180218135555_add_phonenumber_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPhonenumberToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :phonenumber, :string
-    add_index :users, :phonenumber, unique: true
-  end
-end
--- db/migrate/20180218140726_remove_username_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUsernameFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :username, :string
-    remove_column :users, :login, :string
-    remove_column :users, :login_number, :string
-  end
-end
