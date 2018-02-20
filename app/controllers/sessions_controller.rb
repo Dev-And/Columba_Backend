c86be935107d96a3836773edcd9421dfefecb079
Dev-And <raptor131296@gmail.com>
2018-02-20 23:24:12 +0300

    new sign_in response

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,56 +1,16 @@
-class SessionsController < ::DeviseTokenAuth::SessionsController
-  def create
-    # Check
-    field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
+class SessionsController < DeviseTokenAuth::SessionsController
 
-    @resource = nil
-    if field
-      q_value = resource_params[field]
+  def render_create_success
+    token = @token
+    render json: {
+        #data: resource_data(resource_json: @resource.token_validation_response),
+        email: @resource.email,
+        phone: @resource.phone,
+        name:  @resource.name,
+        uid:   @resource.uid,
+        access_token: @token,
+        client: @client_id
+    }
 
-      if resource_class.case_insensitive_keys.include?(field)
-        q_value.downcase!
-      end
-
-      q = "#{field.to_s} = ? AND provider='email'"
-
-      if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? "mysql"
-        q = "BINARY " + q
-      end
-
-      # LOG IN BY EMAIL AND USERNAME
-      if field == :login_number
-        @resource = resource_class.where("email = ? OR login_number = ?", q_value, q_value).first
-      else
-        @resource = resource_class.where(q, q_value).first
-      end
-      # LOG IN BY EMAIL AND USERNAME END
-    end
-
-    if @resource && valid_params?(field, q_value) && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
-      valid_password = @resource.valid_password?(resource_params[:password])
-      if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? { valid_password }) || !valid_password
-        render_create_error_bad_credentials
-        return
-      end
-      # create client id
-      @client_id = SecureRandom.urlsafe_base64(nil, false)
-      @token     = SecureRandom.urlsafe_base64(nil, false)
-
-      @resource.tokens[@client_id] = {
-          token: BCrypt::Password.create(@token),
-          expiry: (Time.now + ::DeviseTokenAuth.token_lifespan).to_i
-      }
-      @resource.save
-
-      sign_in(:user, @resource, store: false, bypass: false)
-
-      yield @resource if block_given?
-
-      render_create_success
-    elsif @resource && !(!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
-      render_create_error_not_confirmed
-    else
-      render_create_error_bad_credentials
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-  mount_devise_token_auth_for 'User', at: 'auth'#,  :controllers => {sessions: 'sesssions'}
+  mount_devise_token_auth_for 'User', at: 'auth',  :controllers => {sessions: 'sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
