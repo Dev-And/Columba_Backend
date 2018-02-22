4fc712984a9e263b560fb8208f2f417e29e46cda
Dev-And <raptor131296@gmail.com>
2018-02-22 15:31:54 +0300

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    render json: {
+        user:  @user.name,
+        email: @user.email,
+        phone: @user.phone,
+        name:  @user.name,
+        uid:   @user.uid,
+        image: @user.image_data,
+        #
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   mount_devise_token_auth_for 'User', at: 'auth',  :controllers => {sessions: 'sessions'}
+    get 'users/:id', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
