7c7ab184ec2a5247a1793c49f7e53450a8c073f6
Dev-And <raptor131296@gmail.com>
2018-02-22 15:39:34 +0300

    user show id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,9 @@ class SessionsController < DeviseTokenAuth::SessionsController
 
   def render_create_success
     render json: {
+
         #data: resource_data(resource_json: @resource.token_validation_response),
+        id:   @resource.id,
         email: @resource.email,
         phone: @resource.phone,
         name:  @resource.name,
